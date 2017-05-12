9431bf669ec02742de9733701ba58ed975aac098
lqlongli(黎清龙) <lqlongli@tencent.com>
2017-05-12 18:00:52 +0800

    feat(1.0.0): init add fixAddSameNameRequire

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # fis3-hook-fix
 fix something of fis3 build
+
+## fix addSameNameRequire
+fis 没有提供接口设置文件的后缀名，默认把 `xxx.yy` 的 `.yy` 作为文件后缀名<br>
+这也没什么错误，但是 fis 有一个逻辑是 `addSameNameRequire`(通过设置 `useSameNameRequire`)<br>
+它会把同名的关联文件添加依赖，比如依赖 `index.js` 模块，也会同时依赖 `index.css`(如果存在的话)<br>
+这个方式在实现的时候，会把文件名去掉后缀名，然后在添加需要同名依赖的类型，然后去找文件<br>
+因为后缀名的关系，如果希望 `index.es6.js` 同名依赖 `index.css` 的话，就有问题了<br>
+因此通过此插件 fix 这个问题，例子如下：
+```
+fis.hook('commonjs', {
+    fixAddSameNameRequire: ['.es6.js']
+});
+```
---
+++ index.js
@@ -0,0 +1,98 @@
+/**
+ * fix something
+ * 1. addSameNameRequire，fix such as 'es6.js'
+ * example:
+ * fis.hook('commonjs', {
+ *     fixAddSameNameRequire: ['.es6.js']
+ * });
+ * 
+ * @author lqlongli
+ * 
+ */
+
+var path = require('path');
+var fs = require('fs');
+
+/*
+ * 后缀规范函数，保证后缀串第一个字符必为.
+ * @param  {String} ext 后缀串
+ * @return {String}     若第一个没有.则加上，若有.则去掉
+ */
+function normalizeExt(ext) {
+    if (ext[0] !== '.') {
+        ext = '.' + ext;
+    }
+    return ext;
+}
+
+/**
+ * 大小写敏感的文件路径判断，fs 没有提供更直接的 API，只能通过 list 接口来判断。
+ */
+function caseSensiveFileExits(filepath) {
+  var dir = path.dirname(filepath);
+  var basename = path.basename(filepath);
+
+  var filenames = fs.readdirSync(dir);
+  if (~filenames.indexOf(basename)) {
+    return true;
+  }
+
+  return false;
+}
+
+function addSameNameRequire(fis, file, realpathNoExt, filename, ext) {
+    var path, map;
+
+    // console.log('>>>> addSameNameRequire:', file.dirname, file.realpath, realpathNoExt, ext);
+    if (fis.util.isFile(realpathNoExt + ext) && caseSensiveFileExits(realpathNoExt + ext)) {
+        path = './' + filename + ext;
+    } else if ((map = fis.media().get('project.ext'))) {
+        for (var key in map) {
+            if (map.hasOwnProperty(key)) {
+                var oExt = normalizeExt(key);
+                var rExt = normalizeExt(map[key]);
+                if (rExt === ext && fis.util.isFile(realpathNoExt + oExt) && caseSensiveFileExits(realpathNoExt + oExt)) {
+                    path = './' + filename + oExt;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (path) {
+        var info = fis.uri.getId(path, file.dirname);
+        if (info.file && info.file.useMap && !~file.asyncs.indexOf(info.id)) {
+            file.addLink(info.file.subpath);
+            file.addRequire(info.id);
+        }
+    }
+}
+
+module.exports = function(fis, opts) {
+    opts = opts || {};
+
+    if (opts.fixAddSameNameRequire && opts.fixAddSameNameRequire.length) {
+        fis.on('process:end', function(file) {
+            var ext;
+            var pos;
+            var realpathNoExt;
+            var filename;
+            if (file.useSameNameRequire) {
+                for (var i = 0, l = opts.fixAddSameNameRequire.length; i < l; ++i) {
+                    ext = normalizeExt(opts.fixAddSameNameRequire[i]);
+                    if ((pos = file.basename.lastIndexOf(ext)) > -1) {
+                        filename = file.basename.substring(0, pos);
+                        realpathNoExt = fis.util(file.dirname, filename);
+                        if (file.isJsLike) {
+                            addSameNameRequire(fis, file, realpathNoExt, filename, '.css');
+                        } else if (file.isHtmlLike) {
+                            addSameNameRequire(fis, file, realpathNoExt, filename, '.js');
+                            addSameNameRequire(fis, file, realpathNoExt, filename, '.css');
+                        }
+                        break;
+                    }
+                }
+            }
+        });
+    }
+};
